99de7079aecc49a74a2847562cc4f72a1a365de3
Vikki Read <hello@whelm.co.uk>
2020-07-26 20:00:34 +0100

    E2e test for multiple robots ignoring bounds case

--- test/acceptance.js
+++ test/acceptance.js
@@ -21,6 +21,21 @@ describe('calculateRobotPosition', async function() {
         });
     });
 
+    context('with multiple robots, ignoring bounds, test case', function() {
+        let sampleInput, sampleOutput;
+
+        before(async function() {
+            sampleInput = await fsPromise.readFile('test/data/main/input-multiple-robots-ignore-bounds.txt', 'utf8');
+            sampleOutput = await fsPromise.readFile('test/data/main/output-multiple-robots-ignore-bounds.txt', 'utf8');
+        });
+    
+        it('returns the sample output when provided with the sample input', async function() {
+            const actual = calculateRobotPosition(sampleInput);
+    
+            expect(sampleOutput).to.equal(actual);
+        });
+    });
+
     context.skip('with full test case', function() {
         let sampleInput, sampleOutput;
 
