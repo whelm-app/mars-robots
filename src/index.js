00691f0f6e957d80a02da4746101c1c527acd6be
Vikki Read <hello@whelm.co.uk>
2020-07-26 20:24:33 +0100

    Refactor into smaller funcs to make improve readability

--- src/index.js
+++ src/index.js
@@ -1,23 +1,48 @@
 const parser = require('./parser');
 const command = require('./command');
 
+const isBecomingLost = (position, world, alreadyLost) => {
+    return !alreadyLost && (position.x > world.width || position.y > world.height);
+}
+
+const findMatchingScent = (scents, position) => {
+    return scents.find(s => s.x === position.x && s.y === position.y);
+}
+
 module.exports = {
     calculateRobotPosition: function(input) {
         const world = parser.parseInput(input);
 
         const instructions = parser.parseAllInstructions(input);
 
+        const scents = [];
         return world.robots.map((robot, i) => {
             const instructionList = instructions[i];
 
-            const out = instructionList.reduce((acc, val) => {
-                const fn = command.COMMAND_MAP[val];
-                const newPosition = fn(acc);
+            const out = instructionList.reduce((previousPosition, instruction) => {
+                const commandFn = command.COMMAND_MAP[instruction];
+                let newPosition = commandFn(previousPosition);
+                
+                let becomingLost = isBecomingLost(newPosition.position, world, previousPosition.lost);
+                
+                if (becomingLost) {
+                    const matchingScent = findMatchingScent(scents, newPosition.position); 
+                    if (matchingScent) {
+                        newPosition = previousPosition;
+                        becomingLost = false;
+                    } else {
+                        scents.push(newPosition.position);
+                    }
+                }
 
-                return newPosition;
+                return {
+                    ...newPosition,
+                    lost: previousPosition.lost || becomingLost
+                };
             }, robot);
 
-            return `${out.position.x} ${out.position.y} ${out.orientation}`;
+            const lost = out.lost ? ' LOST': '';
+            return `${out.position.x} ${out.position.y} ${out.orientation}${lost}`;
         }).join('\n');
 
     }
