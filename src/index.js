1cd6272f318c6312ea8aa0dcad8a072db397cd61
Vikki Read <hello@whelm.co.uk>
2020-07-26 15:06:40 +0100

    Happy path for parsing robot position

--- src/index.js
+++ src/index.js
@@ -6,5 +6,17 @@ module.exports = {
             width: parseInt(inputArray[0], 10),
             height: parseInt(inputArray[1], 10)
         }
+    },
+    parsePosition: function(input) {
+        const inputArray = input.split(' ');
+        const position = {
+            x: parseInt(inputArray[0], 10),
+            y: parseInt(inputArray[1], 10)
+        }
+        const orientation = inputArray[2];
+        return {
+            position,
+            orientation
+        };
     }
 };
--- test/unit/marsRobotSpec.js
+++ test/unit/marsRobotSpec.js
@@ -1,7 +1,36 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const { parseWorld } = require('../../src');
+const { parseWorld, parsePosition } = require('../../src');
+
+describe('parsePosition', async function() {
+    it('returns a robot position when provided with a valid input line', async function() {
+        const x = 3;
+        const y = 5;
+        const orientation = 'N';
+        const input = `${x} ${y} ${orientation}`;
+        
+        const actual = parsePosition(input);
+
+        const expectedOutput = {
+            position: {
+                x,
+                y
+            },
+            orientation
+        };
+
+        expect(expectedOutput).to.deep.equal(actual);
+    });
+
+    describe.skip('handles errors gracefully', function() {
+        it('when input is delimited with tabs');
+        it('when input has extra data after required input');
+        it('when input is missing fields');
+        it('when input is not 2 integers and a char');
+        it('when orientation does not match known orientations');
+    });
+});
 
 describe('parseWorld', async function() {
     it('returns a world object when provided with a valid input line', async function() {
@@ -24,4 +53,11 @@ describe('parseWorld', async function() {
         it('when input is missing fields');
         it('when input is not 2 integers');
     });
+});
+
+describe.skip('parseInput', function() {
+    it('when there is no world line');
+    it('when there is a robot position without instruction set');
+    it('when there is a instruction set without robot position');
+    it('when robot co-ordinates are not within world area');
 });
